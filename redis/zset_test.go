be7e6d97464faa3ef8e5d8b9550a8e9815061cd3
jinrunsen <gasxia@126.com>
2020-04-26 14:26:00 +0800

    [dev] test zset

--- redis/zset_test.go
+++ redis/zset_test.go
@@ -5,6 +5,39 @@ import (
 	"testing"
 )
 
+func TestZaddCommand(t *testing.T) {
+	//score := 1.0
+	member := createStringObject("a")
+	zsl := NewSkiplist()
+	//zsl.Insert(score, member)
+	zs := &zset{
+		dict: &dict{
+			ht: map[interface{}]interface{}{
+			},
+		},
+		zsl: zsl,
+	}
+	zobj := createZsetObject(zs)
+
+	dt := make(map[*robj]*robj)
+	key := createStringObject("abc")
+	dt[key] = zobj
+	db := &Db{
+		id: 1,
+		dict: dt,
+	}
+	c := &client{
+		db: db,
+		argv: []*robj{nil, key, member},
+	}
+
+	zaddCommand(c)
+	rs := replyBuf
+	if rs != "$1\r\n1\r\n" {
+		fmt.Printf("reply: %q\n", rs)
+		t.Error("not equal")
+	}
+}
 func TestZscoreCommand(t *testing.T) {
 	score := 1.0
 	//sobj := createStringObjectFromFloat64(score)
